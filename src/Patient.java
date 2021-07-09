92a6956f6a25a99699d0eabab96a2d3dfc5e14be
Overlord <leon.pa.adolfo@gmail.com>
2021-07-09 12:33:02 -0500

    Patient (#10 Reto) Se crea la class Patient

---
+++ src/Patient.java
@@ -0,0 +1,17 @@
+public class Patient {
+    static int id;
+    String name, email, address, phoneNumber, birthday, blood;
+    double weight, height;
+
+    /**Metodo constructos de Patient
+     * Imprimir el nombre e Email
+     * @param name Es el nombre del Paciente
+     * @param email Es el email del Paciente
+     */
+    Patient(String name, String email){
+        this.name = name;
+        this.email = email;
+        System.out.println("Nombre: " + name + "Email: " + email);
+        id++;
+    }
+}
