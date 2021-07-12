c5b16d5677cafb84f56e00621d0605eb1f2d7073
Overlord <leon.pa.adolfo@gmail.com>
2021-07-12 13:32:22 -0500

    Complete (#37) Terminando la UI de Pacientes

--- src/model/Patient.java
+++ src/model/Patient.java
@@ -1,9 +1,32 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Patient extends User{
 
     private String birthday, blood;
     private double weight, height;
+    private ArrayList<AppointmentDoctor> appointmentDoctors =new ArrayList<>();
+    private ArrayList<AppointmentNurse> appointmentNurses = new ArrayList<>();
+
+    public ArrayList<AppointmentDoctor> getAppointmentDoctors() {
+        return appointmentDoctors;
+    }
+
+    public void addAppointmentDoctors(Doctor doctor, Date date, String time) {
+        AppointmentDoctor appointmentDoctor = new AppointmentDoctor(this,doctor);
+        appointmentDoctor.schedule(date,time);
+        appointmentDoctors.add(appointmentDoctor);
+    }
+
+    public ArrayList<AppointmentNurse> getAppointmentNurses() {
+        return appointmentNurses;
+    }
+
+    public void setAppointmentNurses(ArrayList<AppointmentNurse> appointmentNurses) {
+        this.appointmentNurses = appointmentNurses;
+    }
 
     /**Metodo constructor de model.Patient
      * Imprime el nombre e Email
